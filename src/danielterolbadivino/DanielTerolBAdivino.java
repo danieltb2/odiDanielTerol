0c9d4e1bce94ad431d0a20b084343e227751a908
danie <danie@DESKTOP-L2GL6VL>
2019-10-22 16:28:08 +0200

    cambio terminado

--- src/danielterolbadivino/DanielTerolBAdivino.java
+++ src/danielterolbadivino/DanielTerolBAdivino.java
@@ -15,7 +15,7 @@ public class DanielTerolBAdivino {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = ((int) (40 *Math.random()))+10;
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
